b2483782932882952d1173202ffac7f622e8bc85
MarinV0321 <marin.lff@libero.it>
2021-02-24 19:27:01 +0200

    New print

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello world!2");
+        System.out.println("Marin");
     }
 }
